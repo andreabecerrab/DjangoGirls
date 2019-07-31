431f0b0e97914a6b0d7eb0197877cb3aa713dd16
andreabecerrab <andii1wiki@gmail.com>
2019-07-31 00:02:55 -0500

    heroku settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangogirls',
-        'USER': 'postgres',
-        'PASSWORD': 'helsinki',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
